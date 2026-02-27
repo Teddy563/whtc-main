29e6f7aaba5966c6a5ab6da82d5c618613a88a70
Teddy563 <tediteditu511@gmail.com>
2026-02-27 12:28:44 +0200

    fix: seo, turnstile, responsive images

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://safetyacademy.ro', // or another appropriate URL if known, I'll use the domain from email office@safetyacademy.ro
+  site: 'https://www.whtc.ro', // or another appropriate URL if known, I'll use the domain from email office@safetyacademy.ro
   integrations: [tailwind(), sitemap(), partytown(), icon()],
   image: {
     domains: ["images.unsplash.com"],

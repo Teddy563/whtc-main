322ac36aad0ef466f9bc598f6c2fdaa7ef3def6b
Teddy563 <tediteditu511@gmail.com>
2026-02-27 12:52:46 +0200

    chore: remove partytown integration, use native script loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.whtc.ro', // or another appropriate URL if known, I'll use the domain from email office@safetyacademy.ro
-  integrations: [tailwind(), sitemap(), partytown(), icon()],
+  integrations: [tailwind(), sitemap(), icon()],
   image: {
     domains: ["images.unsplash.com"],
   },
